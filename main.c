00461b46e025bcd59805f0dd195581fbbd196374
CamTosh <camilleminecraft@gmail.com>
2015-11-24 19:46:50 +0100

    Petite update avec deux, trois modifications. Par contre on peut dire bonjour à l'erreur de segmentation (core dumped)

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define nbreEntree 10042
 
 /* ************
 *      _   __
@@ -10,16 +11,16 @@
 *     '-----'`
  ************ */
 
-void mapper(char *data){
-    char mot[255][255];
-    char tmp[255];
+void mapper(char data[]){
+    char mot[nbreEntree][nbreEntree]= {0, 0};
+    char tmp[nbreEntree] = {0};
     int i = 0;
     int j = 0;
     int k = 0; 
     int l = 0;
     int count; 
 
-	data[strlen(data) - 1] = 0; // http://www.tutorialspoint.com/c_standard_library/c_function_strlen.htm
+    data[strlen(data) - 1] = 0; // http://www.tutorialspoint.com/c_standard_library/c_function_strlen.htm
 
     // Le foutoir complet
     while (data[i] != 0) {
@@ -64,43 +65,43 @@ void mapper(char *data){
     }
 }
 
-int main(int argc, char** argv) {    
-	FILE* f = fopen("toast.txt", "r"); // toast.txt = petit fichier à la con, liste.txt -> fichier badass avec 10 000 entrées random sur 3 lettres.
-	if(f == 0) {
-		fclose(f);
-	}
+int main() {    
+    FILE* f = fopen("toast.txt", "r"); // toast.txt = petit fichier à la con, liste.txt -> fichier badass avec 10 000 entrées random sur 3 lettres.
+    if(f == 0) {
+        fclose(f);
+    }
 
-	fseek(f, 0, SEEK_END); // Tu place le "curseur" du fichier a la fin (ça sert a savoir quelle taille fait le fichier)
-	size_t size = ftell(f); // Récupérer la position courante du curseur, donc la taille du fichier vu qu'on a mis le curseur à la fin (et stocker le resultat dans size)
-	fseek(f, 0, SEEK_SET); // Remettre le curseur au début du fichier pour commencer à la lire
+    fseek(f, 0, SEEK_END); // Tu place le "curseur" du fichier a la fin (ça sert a savoir quelle taille fait le fichier)
+    size_t size = ftell(f); // Récupérer la position courante du curseur, donc la taille du fichier vu qu'on a mis le curseur à la fin (et stocker le resultat dans size)
+    fseek(f, 0, SEEK_SET); // Remettre le curseur au début du fichier pour commencer à la lire
 
-	char* data = malloc(size + 1);
-	/*
-	Demander à l'OS 'size + 1' octets de mémoire pour stocker toutes les données du fichier.
-	data est un pointeur vers de l'espace RAM disposible qu'on a demandé à l'OS, c'est comme un tableau de char de taille 'size + 1'
-	Le +1 est lié au fait qu'en C, les strings sont délimités par le caractère '\0' (0x0 en ASCII) afin de savoir où se trouve la fin du string
-	Il nous faut donc 'size' octets pour les données du fichier, et 1 octet de plus pour le caractère '\0'
-	*/
+    char* data = malloc(size + 1);
+    /*
+    Demander à l'OS 'size + 1' octets de mémoire pour stocker toutes les données du fichier.
+    data est un pointeur vers de l'espace RAM disposible qu'on a demandé à l'OS, c'est comme un tableau de char de taille 'size + 1'
+    Le +1 est lié au fait qu'en C, les strings sont délimités par le caractère '\0' (0x0 en ASCII) afin de savoir où se trouve la fin du string
+    Il nous faut donc 'size' octets pour les données du fichier, et 1 octet de plus pour le caractère '\0'
+    */
 
-	size_t red = fread(data, 1, size, f);
-	/*
-	On lit 'size' éléments de taille '1' octet (deuxième argument) à l'emplacement 'data', c'est a dire au début du tableau (la mémoire qu'on a demandé à l'OS)
-	data[size] = '\0'; // On place le caractère '\0' à la toute fin du tableau pour délimiter la fin du string
-	Le tableau est de taille 'size + 1', donc 'data[size]' correspond à la dernière case du tableau (la prenière case est 'data[0]', et pas 'data[1]' !!)
-	La variable 'red' contient le nombre d'octets qu'on a lu au final
-	*/
+    size_t red = fread(data, 1, size, f);
+    /*
+    On lit 'size' éléments de taille '1' octet (deuxième argument) à l'emplacement 'data', c'est a dire au début du tableau (la mémoire qu'on a demandé à l'OS)
+    data[size] = '\0'; // On place le caractère '\0' à la toute fin du tableau pour délimiter la fin du string
+    Le tableau est de taille 'size + 1', donc 'data[size]' correspond à la dernière case du tableau (la prenière case est 'data[0]', et pas 'data[1]' !!)
+    La variable 'red' contient le nombre d'octets qu'on a lu au final
+    */
 
-	if(red != size) { // Si on a pas lu 'size' octets, c'est qu'il y a eu une erreur
-		fclose(f);
-	}
+    if(red != size) { // Si on a pas lu 'size' octets, c'est qu'il y a eu une erreur
+        fclose(f);
+    }
 
-	mapper(data); // L'appel de la fonction mapper avec en paramétre data
+    mapper(data); // L'appel de la fonction mapper avec en paramétre data
 
-	printf("\nDebug :\t Le fichier contient %zu octets \n", size); // Il me semble que le "%zu" ne peut être qu'utiliser en compilant via gcc
-	//printf("Son contenu est: %s \n", data);
+    printf("\nDebug :\t Le fichier contient %zu octets \n", size); // Il me semble que le "%zu" ne peut être qu'utiliser en compilant via gcc
+    //printf("Son contenu est: %s \n", data);
 
-	fclose(f); // On a fini avec le fichier, donc on le ferme, pask faut toujours fermer les fichiers, parce que tg c kom sa.
-	free(data); // On libère la mémoire qu'on avait demandé à l'OS vu qu'on en a plus besoin.
+    fclose(f); // On a fini avec le fichier, donc on le ferme, pask faut toujours fermer les fichiers, parce que tg c kom sa.
+    free(data); // On libère la mémoire qu'on avait demandé à l'OS vu qu'on en a plus besoin.
 
-	return 0;
+    return 0;
 }
