97550fc2db9852884ac0fd9428459cf36572aed1
CamTosh <camilleminecraft@gmail.com>
2015-12-08 21:04:50 +0100

    Toast de foncitonnement ok

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define nbreEntree 10000
+#include <ctype.h>
 
 /* ************
 *      _   __
@@ -13,32 +13,30 @@
 
 
 void mapper(char data[]){
-    char mot[nbreEntree][nbreEntree]= {0, 0};
-    char tmp[nbreEntree] = {0};
+    char mot[50000][30]= {0, 0}; // 42 000 mots avec 30 lettres au max
+
+    char *tmp;
+    tmp  = (char *) malloc(255); // OKLM Le ptit malloc des familles
+
+    // Ahhhhh les compteurs
     int i = 0;
     int j = 0;
     int k = 0;
     int l = 0;
     int count;
 
-    data[strlen(data) - 1] = 0; // http://www.tutorialspoint.com/c_standard_library/c_function_strlen.htm
-
     // Le foutoir complet
-    while (data[i] != '\0'){
-
-        if (data[i] == ' '){
-            mot[j][k] = '\0';
-            k = 0;
-            j++;
-        } 
-        else {
-            mot[j][k++] = data[i];
-        }
+        while (i < 50000){
+            if (!isalpha(data[i])){ // Si le caractère n'est pas une lettre, on démarre un nouveau mot
+                k = 0;
+                j++;
+                l=j;
+                }
+            else {
+                mot[j][k++] = data[i];
+            }
             i++;
-    }
-    
-    mot[j][k] = 0;
-    l = j;
+        }
 
     // La partie de tri
     for (i = 0; i < l; i++) {
@@ -63,15 +61,34 @@ void mapper(char data[]){
                 }
             }
         }
-        printf("\n %s \t %d", mot[i], count);
+        printf("\n %d \t %s", count, mot[i]);
         // Une "incrémentation" de i avec "count" à la place de plus 1, pour passer au mot suivant
         i = i + count;
     }
 }
 
+/* à finir, fonction recherche
+void value(char recherche, char data[], size_t size){
+    int i;
+    char search;
+
+    for(i = 0; i <= size; i++){
+
+        search = strstr(data[i], recherche);
+        if (search != NULL){
+            printf("Le mot apparait dans le fichier \n");
+        }
+        if(search == NULL) {
+            printf("Aucun mot \n");
+        }
+    }
+}
+*/
+
 int main() {
-    FILE* f = fopen("toast.txt", "r"); // toast.txt = petit fichier à la con, liste.txt -> fichier badass avec 10 000 entrées random sur 3 lettres.
-    if(f == 0) {
+    FILE* f = fopen("aliceInWonderland.txt", "r"); // fichier.txt, trainspotting.txt, liste.txt, cupcake.txt
+
+    if(f == 0) { // Si le fichier est = à 0 (null) on le ferme, ça veut dire qu'il est vide, 0 octet, kedal, nadda
         fclose(f);
     }
 
@@ -79,7 +96,8 @@ int main() {
     size_t size = ftell(f); // Récupérer la position courante du curseur, donc la taille du fichier vu qu'on a mis le curseur à la fin (et stocker le resultat dans size)
     fseek(f, 0, SEEK_SET); // Remettre le curseur au début du fichier pour commencer à la lire
 
-    char* data = malloc(size + 1);
+    char* data = (char*)malloc(size + 1); // Ptit malloc tran-tran
+
     /*
     Demander à l'OS 'size + 1' octets de mémoire pour stocker toutes les données du fichier.
     data est un pointeur vers de l'espace RAM disposible qu'on a demandé à l'OS, c'est comme un tableau de char de taille 'size + 1'
@@ -101,8 +119,17 @@ int main() {
 
     mapper(data); // L'appel de la fonction mapper avec en paramétre data
 
-    printf("\nDebug :\t Le fichier contient %zu octets \n", size); // Il me semble que le "%zu" ne peut être qu'utiliser en compilant via gcc
-    //printf("Son contenu est: %s \n", data);
+    /*
+    // Pour la fonction "recherche", ça serai sympa de l'implementer !
+    char recherche;
+
+    printf("\n Recherche : ");
+    scanf("%c", &recherche);
+
+    value(recherche, data, size); // L'appel de la fonction boolean "value" avec en paramétre recherche, data et size
+    */
+
+    //printf("\n \n Debug : Le fichier contient %zu octets \n", size); // Il me semble que le "%zu" ne peut être utiliser qu'en compilant via gcc
 
     fclose(f); // On a fini avec le fichier, donc on le ferme, parce qu'il faut toujours fermer les fichiers, pske tg c kom sa.
     free(data); // On libère la mémoire qu'on avait demandé à l'OS vu qu'on en a plus besoin.
